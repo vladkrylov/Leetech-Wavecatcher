f456c8ee4c9a81c46b49d0fde9d6b74f1603de10
unknown <$leetech@PC-LHCB6.lal.in2p3.fr>
2015-07-08 23:31:21 +0200

    waveforms from ch 0 are written to file (737ms per 1000 generated waveforms)

--- waveformssaver.cpp
+++ waveformssaver.cpp
@@ -1,6 +1,8 @@
 #include "waveformssaver.h"
 
 #include <QDebug>
+#include <QString>
+#include <QElapsedTimer>
 
 WaveformsSaver::WaveformsSaver(QObject *parent) : QObject(parent)
 {
@@ -8,10 +10,24 @@ WaveformsSaver::WaveformsSaver(QObject *parent) : QObject(parent)
     if (!txtOutFile->open(QIODevice::WriteOnly | QIODevice::Text))
         qDebug() << "Cannot create a file!";
     out = new QTextStream(txtOutFile);
+
+    QElapsedTimer eltim;
+    eltim.start();
+    for (int i = 0; i < 1000; ++i) {
+        for (int j = 0; j < 1024; ++j) {
+            *out << "-408.104,";
+        }
+        *out << endl;
+    }
+    qDebug() << eltim.elapsed();
 }
 
 void WaveformsSaver::SaveData(const WAVECAT64CH_ChannelDataStruct* channel)
 {
-    qDebug() << channel[0].Peak;
+//    qDebug() << channel[0].Peak;
+//    for(int i = 0; i < channel[0].WaveformDataSize; i++) {
+//        *out << QString::number(channel[0].WaveformData[i]) << ",";
+//    }
+//    *out << endl;
 }
 
